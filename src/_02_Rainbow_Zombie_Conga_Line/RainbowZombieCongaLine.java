5579c1d133eac924f07f1f26ed3be814f53a0783
league <league@imac-24.attlocal.net>
2024-11-01 18:30:14 -0700

    im gonna fail english (to do: fix errors and youaredone in conga line)

--- src/_02_Rainbow_Zombie_Conga_Line/RainbowZombieCongaLine.java
+++ src/_02_Rainbow_Zombie_Conga_Line/RainbowZombieCongaLine.java
@@ -4,81 +4,132 @@ import _00_Intro_to_Linked_Lists.LinkedList;
 import _00_Intro_to_Linked_Lists.Node;
 
 public class RainbowZombieCongaLine {
-    
-    /*
-     * You are hosting a rainbow zombie conga dance party! Zombies are not very
-     * smart(maybe that's why they crave brains) and need your help to direct
-     * them through the choreography.
-     * 
-     * Each method is a dance move you will need to implement.
-     * 
-     * When you think you've gotten your zombies trained well enough you can
-     * start the party by running the main method in RainbowZombieDanceParty and
-     * typing how many rounds you want in the console to see if they followed
-     * your choreography correctly.
-     * 
-     * Note: The party will always start with a rainbow brains and every 5
-     * rounds the head and tail of the dance line will be removed.
-     */
 
-    private LinkedList<Zombie> congaLine;
-    private ZombieHatColor[] zombieHats;
+	/*
+	 * You are hosting a rainbow zombie conga dance party! Zombies are not very
+	 * smart(maybe that's why they crave brains) and need your help to direct
+	 * them through the choreography.
+	 * 
+	 * Each method is a dance move you will need to implement.
+	 * 
+	 * When you think you've gotten your zombies trained well enough you can
+	 * start the party by running the main method in RainbowZombieDanceParty and
+	 * typing how many rounds you want in the console to see if they followed
+	 * your choreography correctly.
+	 * 
+	 * Note: The party will always start with a rainbow brains and every 5
+	 * rounds the head and tail of the dance line will be removed.
+	 */
 
-    public RainbowZombieCongaLine() {
+	private LinkedList<Zombie> congaLine;
+	private ZombieHatColor[] zombieHats;
+	Node<Zombie> head;
 
-        congaLine = new LinkedList<Zombie>();
-        zombieHats = ZombieHatColor.values();
+	public RainbowZombieCongaLine() {
 
-    }
+		congaLine = new LinkedList<Zombie>();
+		zombieHats = ZombieHatColor.values();
 
-    // Make the passed in zombie the first Zombie in the conga line!
-    public void engine(Zombie dancer) {
+		head = congaLine.getHead();
 
-    }
+	}
 
-    // Make the passed in zombie the last Zombie in the conga line!
-    public void caboose(Zombie dancer) {
+	// Make the passed in zombie the first Zombie in the conga line!
+	public void engine(Zombie dancer) {
+		congaLine.getHead().setValue(dancer);
+	}
 
-    }
+	// Make the passed in zombie the last Zombie in the conga line!
+	public void caboose(Zombie dancer) {
+		congaLine.getTail().setValue(dancer);
+	}
 
-    // Place the zombie at the designated position in the conga line!
-    public void jumpInTheLine(Zombie dancer, int position) {
+	// Place the zombie at the designated position in the conga line!
+	public void jumpInTheLine(Zombie dancer, int position) {
+		for (int i = 0; i < position; i++) {
+			if (i==position-1) {
+				head.setValue(dancer);
+			}
+			head = head.getNext();
+		}
+		head=congaLine.getHead();
+	}
 
-    }
+	/*
+	 * Remove all zombies with the same hat color as the passed in zombie from
+	 * the conga line!
+	 */
+	public void everyoneOut(Zombie dancer) {
+		for (int i = 0; i < congaLine.size(); i++) {
+			if (head.getValue().getZombieHatColor() == dancer.getZombieHatColor()) {
+				congaLine.remove(i);
+				i--;
+			}
+			head = head.getNext();
+		}
+		head=congaLine.getHead();
+	}
 
-    /*
-     * Remove all zombies with the same hat color as the passed in zombie from
-     * the conga line!
-     */
-    public void everyoneOut(Zombie dancer) {
+	/*
+	 * Remove the first zombie with the same hat color as the passed in zombie
+	 * from the conga line!
+	 */
+	public void youAreDone(Zombie dancer) {
+		boolean found = false;
+		int position = 0;
+		while (found=false) {
+			if (head.getValue().getZombieHatColor()==dancer.getZombieHatColor()) {
+				congaLine.remove(position);
+				found=true;
+			} else {
+				position++;
+				head=head.getNext();
+			}
+		}
+		head=congaLine.getHead();
+	}
 
-    }
+	/*
+	 * Make three more zombies with the same hat color as the passed in zombie and
+	 * add one to the front, one to the end and one in the middle.
+	 */
+	public void brains(Zombie dancer) {
+		if (congaLine.size()%2==0) {
+			for (int i = 0; i < congaLine.size()/2; i++) {
+				head = head.getNext();
+			}
+		} else {
+			for (int i = 0; i < congaLine.size()/2+1; i++) {
+				head = head.getNext();
+			}
+			head.setValue(dancer);
+		}
+		congaLine.getHead().setValue(dancer);
+		congaLine.getTail().setValue(dancer);
 
-    /*
-     * Remove the first zombie with the same hat color as the passed in zombie
-     * from the conga line!
-     */
-    public void youAreDone(Zombie dancer) {
+		head=congaLine.getHead();
+	}
 
-    }
+	/*
+	 * Add the passed in zombie to the front and then add one zombie of each hat
+	 * color to the end of the line.
+	 */
+	public void rainbowBrains(Zombie dancer) {
+		//congaLine.getHead().setValue(dancer);
+		//NOTE: this line doesn't work so fix it later
 
-    /*
-     * Make two more zombies with the same hat color as the passed in zombie and
-     * add one to the front, one to the end and one in the middle.
-     */
-    public void brains(Zombie dancer) {
+		congaLine.add(new Zombie(ZombieHatColor.R));
+		congaLine.add(new Zombie(ZombieHatColor.O));
+		congaLine.add(new Zombie(ZombieHatColor.Y));
+		congaLine.add(new Zombie(ZombieHatColor.G));
+		congaLine.add(new Zombie(ZombieHatColor.B));
+		congaLine.add(new Zombie(ZombieHatColor.I));
+		congaLine.add(new Zombie(ZombieHatColor.V));
 
-    }
+		head = congaLine.getHead();
+	}
 
-    /*
-     * Add the passed in zombie to the front and then add one zombie of each hat
-     * color to the end of the line.
-     */
-    public void rainbowBrains(Zombie dancer) {
-
-    }
-
-    public LinkedList<Zombie> getCongaLine() {
-        return congaLine;
-    }
+	public LinkedList<Zombie> getCongaLine() {
+		return congaLine;
+	}
 }
